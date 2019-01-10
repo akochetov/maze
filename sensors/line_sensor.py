2805b3d3466ff0459a80664b7ed9c3705cf7a62b
Kochetov <Anatoliy.Kochetov@sigmaukraine.com>
2019-01-10 15:05:51 +0200

    Fixes to get maze map to work

--- sensors/line_sensor.py
+++ sensors/line_sensor.py
@@ -1,40 +1,46 @@
-"""
-Line following IR sensor module, RPi aimed
+from misc.direction import Direction
 
-States table:
-
-State   Interpretation                                  Left motor power in %       Right motor power in %
-00100   Straight or Back (if next is 00000)             20                          20
-01110   Straight or Back (if next is 00000)             20                          20
-
-01000   Left. Much out of line. Fix required            20                          10
-01100   Left. Slightly out of line. Major fix required  20                          15
-00010   Right. Much out of line. Fix required           10                          20
-00110   Right. Slightly out of line. Fix required       15                          20
-
-10000   Left Crossing.                                  5                           20
-11000   Left Crossing.                                  7                           20
-11100   Left Crossing.                                  10                          20
-
-00001   Right Crossing.                                 20                          5
-00011   Right Crossing.                                 20                          7
-00111   Right Crossing.                                 20                          10
-
-11111   T-shaped crossing. Left and Right. Front - ?
-00000   Out of line. Decide basing on prev value.        ?                           ?
-"""
 
 class LineSensor(object):
-	def __init__(self, source):
-		"""
-		param source: instance of LineSensorSourceBase inherited class
-		"""
+    LEFT = ["[1, 1, "]
+    RIGHT = [", 1, 1]"]
+    FORWARD = [", 0, 1, 0, ", ", 1, 1, 1, ", ", 0, 1, 1, ", ", 1, 1, 0, "]
 
-		super().__init__()
+    def __init__(self, source):
+        """
+        param source: instance of LineSensorSourceBase inherited class
+        """
 
-		self.source = source
+        super().__init__()
 
-	def get_state(self):
-		return self.source.get_state()
+        self.source = source
 
+    def __find_direction(self, state, direction):
+        for dir in direction:
+            if state.find(dir) >= 0:
+                return True
 
+        return False
+
+    def get_state(self):
+        return self.source.get_state()
+
+    def get_directions(self):
+        ret = []
+        state = str(self.get_state())
+
+        if self.__find_direction(state, LineSensor.LEFT):
+            ret.append(Direction.LEFT)
+
+        if self.__find_direction(state, LineSensor.RIGHT):
+            ret.append(Direction.RIGHT)
+
+        if self.__find_direction(state, LineSensor.FORWARD):
+            ret.append(Direction.FORWARD)
+
+        return ret
+
+    def is_crossing(self):
+        dirs = self.get_directions()
+
+        return Direction.LEFT in dirs or Direction.RIGHT in dirs or len(dirs) == 0
