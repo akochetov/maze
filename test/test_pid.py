8cd327f4062ab93fc6acec4fc42a1b175e4a25b8
Kochetov <Anatoliy.Kochetov@sigmaukraine.com>
2019-02-19 12:48:58 +0200

    Tests

---
+++ test/test_pid.py
@@ -0,0 +1,69 @@
+import sys
+import os
+import time
+from car import Car
+from sensors.line_sensor import LineSensor
+import misc.settings as settings
+
+# virtual imports
+from worlds.virtual_world import VirtualWorld
+from chassis.virtual_chassis import VirtualChassis
+from sensors.virtual_line_sensor_source import VirtualLineSensorSource
+
+# physical RPi imports
+import RPi.GPIO as GPIO
+from chassis.rpi_2wheels_chassis import RPi2WheelsChassis
+from sensors.rpi_line_sensor_source import RPiLineSensorSource
+from misc.rpi_line_sensor_pid import RPiLineSensorPID
+
+exit_loop = False
+
+
+def exitLoop(arg1, arg2):
+    global exit_loop
+    exit_loop = True
+
+signal(SIGINT, exitLoop)
+signal(SIGTERM, exitLoop)
+
+
+GPIO.setmode(GPIO.BCM)
+
+line_sensor = LineSensor(RPiLineSensorSource(
+        settings.LINE_SENSORS,
+        ORIENTATION,
+        invert=True,
+        signals_window_size=settings.SIGNALS_WINDOWS_SIZE,
+        state_trigger_repetitions=settings.STATE_ACTION_REPETITIONS
+        ),
+        settings.FREQ)
+
+sensor_pid = RPiLineSensorPID(
+        settings.PID,
+        line_sensor,
+        settings.STATE_OK
+    )
+
+chassis = RPi2WheelsChassis(
+        settings.LEFT_MOTOR,
+        settings.RIGHT_MOTOR,
+        settings.LEFT_MOTOR_POWER,
+        settings.RIGHT_MOTOR_POWER,
+        settings.TIME_TO_TURN,
+        settings.BRAKE_TIME,
+        settings.PWM,
+        sensor_pid,
+        settings.FREQ)
+
+car = Car(None, chassis, [line_sensor], ORIENTATION)
+
+car.move()
+while not exit_loop:
+    time.sleep(0.5)
+
+line_sensor.stop()
+car.stop()
+
+print('Test finished.')
+
+GPIO.cleanup()
---
+++ test/test_sensor.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+from sensors.line_sensor import LineSensor
+from sensors.rpi_line_sensor_source import RPiLineSensorSource
+import misc.settings as settings
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+line_sensor = LineSensor(RPiLineSensorSource(
+        settings.LINE_SENSORS,
+        ORIENTATION,
+        invert=True,
+        signals_window_size=settings.SIGNALS_WINDOWS_SIZE,
+        state_trigger_repetitions=settings.STATE_ACTION_REPETITIONS
+        ),
+        settings.FREQ)
+
+print('Testing line sensors. Move sensor line above the line to see readings:')
+for i in range(10000):
+    data = line_sensor.get_state()
+
+    print(bin(data))
+    sleep(0.5)
+
+print('Test finished.')
+
+GPIO.cleanup()
