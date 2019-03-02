4758be9e7db2b6da740130a37f812500c5905f7b
Kochetov <Anatoliy.Kochetov@sigmaukraine.com>
2019-03-02 22:20:30 +0200

    Improved settings for GA12-6-300

--- misc/settings.py
+++ misc/settings.py
@@ -19,8 +19,8 @@ CTRL_LED = 15
 RIGHT_MOTOR = {"EN": 13, "IN1": 5, "IN2": 6}
 LEFT_MOTOR = {"EN": 19, "IN1": 20, "IN2": 21}
 
-LEFT_MOTOR_POWER = {"FAST": 30, "SLOW": 18, "TURN": 26}
-RIGHT_MOTOR_POWER = {"FAST": 34, "SLOW": 22, "TURN": 30}
+LEFT_MOTOR_POWER = {"FAST": 31, "SLOW": 19, "TURN": 26}
+RIGHT_MOTOR_POWER = {"FAST": 34, "SLOW": 22, "TURN": 32}
 
 PWM = 50
 
@@ -30,7 +30,7 @@ LINE_SENSORS = [12, 11, 8, 25, 24, 23, 18]
 # PID coefficients, PK, IK and DK repsectively, then D fading coefficient
 # D fading means that every iteration D error will be multiplied by D fading
 # PID = [1 / 125, 0, 1 / 9]
-PID = [1 / 130, 0, 1 / 14, 0.15]
+PID = [1 / 130, 0, 1 / 14, 0.10]
 
 # state error
 STATE_OK = 3000
@@ -42,7 +42,7 @@ STATE_ACTION_REPETITIONS = 5
 # maze map settings
 TIME_ERROR = 0.5
 TIME_TO_TURN = 0.38
-BRAKE_TIME = 1.0 / 17.0
+BRAKE_TIME = 1.0 / 15.0
 
 # if we have turn very early afte previous one - bounce back (ignore it)
 TURN_BOUNCE_TIME = 1.0
