d8e3d1288560f1a359c1e3717c98304fda7ed22b
Kochetov <Anatoliy.Kochetov@sigmaukraine.com>
2019-03-14 19:42:40 +0200

    Launcher simplifed to send signals to main process when btn is pressed second time

--- main.py
+++ main.py
@@ -21,14 +21,23 @@ from sensors.virtual_line_sensor_source import VirtualLineSensorSource
 ORIENTATION = Orientation.SOUTH
 
 exit_loop = False
+time_to_go = False
 
 
 def exitLoop(arg1, arg2):
     global exit_loop
     exit_loop = True
 
+
+def go(arg1, arg2):
+    global time_to_go
+    time_to_go = True
+
+# setup signals to stop program properly when interrupted by user
 signal(SIGINT, exitLoop)
 signal(SIGTERM, exitLoop)
+# setup signal to launch car at button press
+signal(SIGUSR1, go)
 
 maze_world = None
 chassis = None
@@ -48,13 +57,6 @@ else:
     from sensors.rpi_line_sensor_source import RPiLineSensorSource
     from sensors.spi_line_sensor_source import SPiLineSensorSource
     from misc.rpi_line_sensor_pid import RPiLineSensorPID
-
-    GPIO.setmode(GPIO.BCM)
-
-    GPIO.setup(settings.CTRL_LED, GPIO.OUT)
-    GPIO.output(settings.CTRL_LED, GPIO.HIGH)
-
-    GPIO.setup(settings.CTRL_BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     maze_world = LineWorld()
 
@@ -112,21 +114,12 @@ if settings.NAVIGATE_BACK:
     maze_map = MazeMap(car, settings.TIME_ERROR, settings.TIME_TO_TURN)
 
 if not settings.VIRTUAL and len(sys.argv) <= 1:
-    while (
-        not exit_loop and
-        GPIO.wait_for_edge(
-            settings.CTRL_BTN,
-            GPIO.FALLING,
-            timeout=500) is None):
+    # wait for button to be pressed again to start a car
+    while not exit_loop and not time_to_go:
         pass
-
-    GPIO.cleanup(settings.CTRL_BTN)
 
 start_time = time.time()
 brain.think(maze_map)
-
-if not settings.VIRTUAL:
-    GPIO.output(settings.CTRL_LED, GPIO.LOW)
 
 while not exit_loop:
     if not brain.is_still_thinking() and path_brain is None:
@@ -178,7 +171,3 @@ brain.stop()
 car.stop()
 
 print('All stopped.')
-
-if not settings.VIRTUAL:
-    GPIO.output(settings.CTRL_LED, GPIO.LOW)
-    GPIO.cleanup()
