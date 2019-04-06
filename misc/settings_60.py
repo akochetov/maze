9422eaeb39271dfc5d5d076bd00ad94c37f2142e
kochetov_anatoly@mail.ru <kochetov_anatoly@mail.ru>
2019-04-06 22:56:05 +0100

    60% settings

--- misc/settings_60.py
+++ misc/settings_60.py
@@ -8,7 +8,7 @@ NAVIGATE_BACK = 0
 LOG = 0
 
 # control iterations per second
-CTRL_FREQ = 250
+CTRL_FREQ = 200
 PID_FREQ = 30
 
 # button and LED pins to control robot
@@ -36,13 +36,13 @@ LINE_SENSORS = [12, 11, 8, 25, 24, 23, 18]
 # SPI sensor parameters
 SPI_LINE_SENSOR_CHANNELS = [0, 1, 2, 3, 4, 5, 6]
 SPI_LINE_SENSOR_MIN_MAX = [
-    {"MIN": 977, "MAX": 990},
-    {"MIN": 977, "MAX": 990},
-    {"MIN": 977, "MAX": 990},
-    {"MIN": 970, "MAX": 990},
-    {"MIN": 970, "MAX": 990},
-    {"MIN": 970, "MAX": 990},
-    {"MIN": 970, "MAX": 990}
+    {"MIN": 980, "MAX": 990},
+    {"MIN": 980, "MAX": 990},
+    {"MIN": 980, "MAX": 990},
+    {"MIN": 980, "MAX": 990},
+    {"MIN": 980, "MAX": 990},
+    {"MIN": 980, "MAX": 990},
+    {"MIN": 980, "MAX": 990}
     ]
 
 # PID coefficients, PK, IK and DK repsectively, then D fading coefficient
@@ -53,13 +53,13 @@ PID = [1 / 25, 1 / 10000, 1 / 2.45, 0]
 STATE_OK = 2000
 
 # how many times in a row action has to repeat to count
-SIGNALS_WINDOWS_SIZE = 17
+SIGNALS_WINDOWS_SIZE = 15
 STATE_ACTION_REPETITIONS = 2
 
 # maze map settings
 TIME_ERROR = 0.2
-TIME_TO_TURN = 0.25
-BRAKE_TIME = 1.0 / 11.0
+TIME_TO_TURN = 0.27
+BRAKE_TIME = 1.0 / 13.0
 
 # if we have turn very early afte previous one - bounce back (ignore it)
 TURN_BOUNCE_TIME = 0.45
